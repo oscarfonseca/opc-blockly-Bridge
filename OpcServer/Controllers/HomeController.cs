c6d41caf19e5e02b134b42e654b844b53e876355
Oscar Fonseca <oscar.fonseca@grollmus.de>
2023-08-08 12:55:56 +0200

    Initial Commit

---
+++ OpcServer/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OpcServer.Models;
+
+namespace OpcServer.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+    [HttpGet("read")]
+    public IActionResult Read()
+    {
+        return Ok("Read request handled");
+    }
+    
+    [HttpPost("write")]
+    public IActionResult Write()
+    {
+        return Ok("Write request handled");
+    }
+}
